15e954f61686ff8b0cbff7ae2b0d1356cdc493e6
Varun Chitre <varun.chitre15@gmail.com>
2015-03-25 04:20:49 -0400

    sprout: update fingerprint from Android 5.1

--- full_sprout.mk
+++ full_sprout.mk
@@ -9,5 +9,5 @@ $(call inherit-product, device/google/sprout/sprout.mk)
 
 LOCAL_PATH := device/google/sprout
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=4.4.4/KPW53/1379542:user/release-keys PRIVATE_BUILD_DESC="sprout-user 4.4.4 KPW53 1379542 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1/LMY47O/1783956:user/release-keys PRIVATE_BUILD_DESC="sprout-user 5.1 LMY47O 1783956 release-keys"
 
