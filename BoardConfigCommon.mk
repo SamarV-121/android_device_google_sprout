5598ce7e4d745ba6f9c0da0669863bcfe6c11357
David Marble <drmarble1@gmail.com>
2015-11-25 21:49:52 -0600

    sprout-common: BoardConfigCommon: set TARGET_BOOTANIMATION_HALF_RES := true
    Speeds up boot. Especially first boot.
    Dootanimation cpu usage goes from ~25% to ~7.5%.
    No major decrease in resolution.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,6 +91,9 @@ BLOCK_BASED_OTA := true
 # FRAMEWORK WITH OUT SYNC
 TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
+# Use half res bootanimation to speed up first boot sequence
+TARGET_BOOTANIMATION_HALF_RES := true
+
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
 
