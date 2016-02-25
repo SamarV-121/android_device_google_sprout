01d85bc38f093a6b20e55f1ed6944446d770b616
Lorenzo Colitti <lorenzo@google.com>
2016-02-25 14:52:17 +0900

    sprout: Delete dhcpcd init entries, since dhcpcd is no more.
    Bug: 27192644
    Change-Id: I3a682a96bb14977678159c5bd6ee361b92763905
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -204,7 +204,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     libwpa_client \
     hostapd \
-    dhcpcd.conf \
     wpa_supplicant \
     wpa_supplicant.conf \
     wificond \
