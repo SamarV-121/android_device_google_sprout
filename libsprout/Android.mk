e29dfad02df1b9bf168dceb3be0e289408c735e0
jrior001 <jriordan001@gmail.com>
2016-09-07 21:52:37 -0400

    sprout: Shim missing icu symbols
     * F linker  : CANNOT LINK EXECUTABLE "/system/bin/mtk_agpsd": cannot locate symbol "UCNV_FROM_U_CALLBACK_STOP_55" referenced by "/system/bin/mtk_agpsd"
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- libsprout/Android.mk
+++ libsprout/Android.mk
@@ -6,9 +6,10 @@ LOCAL_SRC_FILES := \
     bionic.cpp \
     ui.cpp \
     camera.cpp \
-    buffer.cpp
+    buffer.cpp \
+    icu55.c
 
-LOCAL_SHARED_LIBRARIES := libbinder libui libgui libcutils libutils libc liblog libstagefright_foundation
+LOCAL_SHARED_LIBRARIES := libbinder libui libgui libcutils libutils libc liblog libstagefright_foundation libicuuc libicui18n
 LOCAL_MODULE := libsprout
 LOCAL_MODULE_TAGS := optional
 
