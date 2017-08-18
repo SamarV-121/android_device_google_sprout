44bec4bc0b28bb1ccaff0e6308886ab691263a13
Shankar D. Warang <shankar.warang9@gmail.com>
2017-08-18 11:38:11 +0000

    Added something essential and missing!

--- sprout.mk
+++ sprout.mk
@@ -55,8 +55,8 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/configs/thermal.conf:system/etc/.tp/thermal.conf
-     $(LOCAL_PATH)/configs/.ht120.mtc:system/etc/.tp/.ht120.mtc
+     $(LOCAL_PATH)/configs/thermal.conf:system/etc/.tp/thermal.conf \
+     $(LOCAL_PATH)/configs/.ht120.mtc:system/etc/.tp/.ht120.mtc \
      $(LOCAL_PATH)/configs/thermal.off.conf:system/etc/.tp/thermal.off.conf
 	
 # Keylayout
