c8ba24ce4f0c63d86372a49e6106b246577f68c6
SamarV-121 <samarvispute121@gmail.com>
2017-08-26 18:24:52 +0530

    sprout-common: bringup DozeServices|Thanks @rohantaneja

--- sprout.mk
+++ sprout.mk
@@ -175,6 +175,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-filter=speed \
     dalvik.vm.dex2oat-swap=false
 
+# Doze
+PRODUCT_PACKAGES += \
+    DozeServices
+
 # MTK Shims
 PRODUCT_PACKAGES += \
     libmtk_symbols \
