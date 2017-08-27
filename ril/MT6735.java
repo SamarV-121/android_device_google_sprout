7a363adc52c3a388a413fdd5f9913503e3ac75e3
Samar Vispute <samarvispute121@gmail.com>
2017-08-27 21:44:55 +0530

    sprout-common: ril: fix invalid number error By @varunchitre15

--- ril/MT6735.java
+++ ril/MT6735.java
@@ -310,6 +310,24 @@ public class MT6735 extends RIL implements CommandsInterface {
         send(rr);
     }
 
+    protected Object
+    responseFailCause(Parcel p) {
+        int numInts;
+        int response[];
+
+        numInts = p.readInt();
+        response = new int[numInts];
+        for (int i = 0 ; i < numInts ; i++) {
+            response[i] = p.readInt();
+        }
+        LastCallFailCause failCause = new LastCallFailCause();
+        failCause.causeCode = response[0];
+        if (p.dataAvail() > 0) {
+          failCause.vendorCause = p.readString();
+        }
+        return failCause;
+    }
+    
     @Override
     public void
     deactivateDataCall(int cid, int reason, Message result) {
