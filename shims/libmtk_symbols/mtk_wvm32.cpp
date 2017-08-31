86dde12b449c7fc3b2173102021b24e529725226
Samar Vispute <samarvispute121@gmail.com>
2017-08-31 11:32:32 +0530

    sprout-common: shims: update symbols For wvm

--- shims/libmtk_symbols/mtk_wvm32.cpp
+++ shims/libmtk_symbols/mtk_wvm32.cpp
@@ -1,16 +1,19 @@
 #include <stdint.h>
 
-extern "C" {
-// int _ZN7android16MediaBufferGroupC1Em(unsigned long);
-int _ZN7android16MediaBufferGroupC1Ej();
-int _ZN7android16MediaBufferGroupC1Ev() {
-    // return _ZN7android16MediaBufferGroupC1Em(0);
-    return _ZN7android16MediaBufferGroupC1Ej();
+extern "C" int _ZN7android16MediaBufferGroupC1Ej(unsigned int);
+extern "C" int _ZN7android16MediaBufferGroupC1Ev() {
+    return _ZN7android16MediaBufferGroupC1Ej(0);
 }
-void _ZN7android14SurfaceControl8setLayerEj(uint32_t);
 
-void _ZN7android14SurfaceControl8setLayerEi(int32_t layer){
-        _ZN7android14SurfaceControl8setLayerEj(static_cast<uint32_t>(layer));
+extern "C" bool _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb();
+extern "C" bool _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb() {
+  return _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb();
+}
+extern "C" void _ZN7android14SurfaceControl8setLayerEj(uint32_t);
+
+extern "C" void _ZN7android14SurfaceControl8setLayerEi(int32_t layer){
+       return _ZN7android14SurfaceControl8setLayerEj(static_cast<uint32_t>(layer));
     }
 
 }
+
