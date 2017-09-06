4143050ff8d731fbd2fa2807ee46140558ee7f1e
Samar Vispute <samarvispute121@gmail.com>
2017-09-06 15:57:21 +0530

    shims: wvm: remove ZN7android14SurfaceControl8setLayerEj symbol

--- shims/libmtk_symbols/mtk_wvm32.cpp
+++ shims/libmtk_symbols/mtk_wvm32.cpp
@@ -9,11 +9,5 @@ extern "C" bool _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBuffer
 extern "C" bool _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb() {
   return _ZN7android16MediaBufferGroup14acquire_bufferEPPNS_11MediaBufferEb();
 }
-extern "C" void _ZN7android14SurfaceControl8setLayerEj(uint32_t);
 
-extern "C" void _ZN7android14SurfaceControl8setLayerEi(int32_t layer){
-       return _ZN7android14SurfaceControl8setLayerEj(static_cast<uint32_t>(layer));
-    }
-
-}
 
