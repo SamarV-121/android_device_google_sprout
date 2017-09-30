2fd666bf3ab5b218c48bf9547a82ad3b550e3bad
Varun Chitre <varun.chitre15@gmail.com>
2017-09-30 18:34:47 +0000

    sprout: build graphics HALs and services

--- sprout.mk
+++ sprout.mk
@@ -155,6 +155,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service
+
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
