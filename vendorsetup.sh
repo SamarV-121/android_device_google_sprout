6ce8d6a13588654b2e98e0a4aca2e4cf8b2fcd36
SamarV-121 <samarvispute121@gmail.com>
2018-01-26 15:43:10 +0530

    sprout-common: patch: fix camera and hotspot
    *  __pthread_gettid libc symbol needed for mm base mtk's libmtkjpeg.so blob ( for working camera)
    * fix hotspot tethering error while data is on. Thanks @Meticulus https://github.com/Meticulus/android_device_huawei_hi6250/blob/n-emui4/patches/common/system/netd/0001-netd-fix-tethering.patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,12 @@ git reset --hard && git clean -f -d
 patch -p1 < ../../device/google/sprout-common/patch/signature_spoofing_permission.patch
 git clean -f
 cd ../..
+cd system/netd
+git reset --hard && git clean -f -d
+patch -p1 < ../../device/google/sprout-common/patch/netd_fix_tethering.patch
+cd ../..
+cd bionic
+git reset --hard && git clean -f -d
+patch -p1 < ../device/google/sprout-common/patch/Apply-LIBC-version-to-__pthread_gettid.patch
+cd ..
+
