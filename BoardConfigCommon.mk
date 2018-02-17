8f1b5be6030d931ffef60815816dd3a5ca26962d
SamarV-121 <samarvispute121@gmail.com>
2018-02-17 09:54:57 +0530

    sprout-common: update doze for lineage-15.1 and define kernel image name
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,6 +52,7 @@ EXTENDED_FONT_FOOTPRINT := true
 # Kernel Stuff
 TARGET_KERNEL_SOURCE := kernel/mediatek/sprout
 TARGET_KERNEL_CONFIG := lineageos_sprout_defconfig
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
