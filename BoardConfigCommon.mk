70995e8dbfb2a89fc5a1b68db6412a489b066988
SamarV-121 <samarvispute121@gmail.com>
2018-02-18 10:45:35 +0530

    sprout-common: bring back SeccompPolicy again and fix mediacodecs denials

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -118,6 +118,6 @@ BOARD_SEPOLICY_DIRS += \
     device/google/sprout-common/sepolicy
 
 # Seccomp Policy
-# BOARD_SECCOMP_POLICY += device/google/sprout-common/seccomp
+BOARD_SECCOMP_POLICY += device/google/sprout-common/seccomp
 
 
