a1cef57d9ec27bd08d67c2b233c21d7a3854405d
Varun Chitre <varun.chitre15@gmail.com>
2018-04-05 18:29:17 +0000

    sprout: enable Android Go
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- sprout.mk
+++ sprout.mk
@@ -14,6 +14,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
