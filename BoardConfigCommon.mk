1959ee4622dc6f4caa2e2345f0966f04f723ded5
SamarV-121 <samarvispute121@gmail.com>
2018-04-06 17:10:36 +0530

    sprout-common: Switch to mke2fs for creating ext4 images
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,7 +44,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 # TARGET_KERNEL_HAVE_EXFAT := true
 # TARGET_KERNEL_HAVE_NTFS  := true
 # Use mke2fs instead of make_ext4fs
-# TARGET_USES_MKE2FS := true
+TARGET_USES_MKE2FS := true
 
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
