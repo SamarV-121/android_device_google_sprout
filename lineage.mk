047ddaeb7e481ca37c57350f279a4b7b14ef6d42
SamarV-121 <samarvispute121@gmail.com>
2019-04-05 07:08:19 +0000

    sprout: Rearrange lineage.mk
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -1,10 +1,17 @@
 $(call inherit-product, device/google/sprout/device.mk)
 
-# Common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_sprout
 PRODUCT_DEVICE := sprout
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
 PRODUCT_MODEL := sprout
+
+# Override Fingerprint
+BUILD_FINGERPRINT := 6.0/MRA58M/2280749:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys" \
+    PRODUCT_NAME=sprout \
+    TARGET_DEVICE=sprout
