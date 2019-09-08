2912d51c55500db411e0469692fd11d4a013abed
erfanoabdi <erfangplus@gmail.com>
2019-09-08 15:04:55 +0430

    sprout: Shim yet another missing symbol
    P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
    Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
    Error: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv" referenced by "/system/lib/libcam_utils.so".
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- libsprout/camera.cpp
+++ libsprout/camera.cpp
@@ -1,6 +1,15 @@
+#include <stdint.h>
 #include <string>
 #include <cutils/native_handle.h>
 #include <ui/GraphicBuffer.h>
+
+namespace android {
+    extern "C" void _ZN7android13GraphicBuffer4lockEjPPvPiS3_(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
+
+    extern "C" void _ZN7android13GraphicBuffer4lockEjPPv(uint32_t inUsage, void** vaddr) {
+        _ZN7android13GraphicBuffer4lockEjPPvPiS3_(inUsage, vaddr, nullptr, nullptr);
+    }
+}
 
 extern "C" void _ZN7android13GraphicBufferC1EjjijjjP13native_handleb(
         const native_handle_t* handle,
