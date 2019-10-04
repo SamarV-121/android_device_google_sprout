f92908d4d50312ef7bc0850860a7e22ad9b3fc03
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    sprout: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -218,7 +218,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
  PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service-legacy \
     libwpa_client \
     hostapd \
     wpa_supplicant \
