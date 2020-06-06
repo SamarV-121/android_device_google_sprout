e1fd2e1d078e1721c3bad9137ecdf80e4b430ee0
SamarV-121 <samarvispute121@gmail.com>
2020-06-07 00:04:22 +0530

    sprout: Remove mtk_agps daemon again
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- libsprout/Android.mk
+++ libsprout/Android.mk
@@ -6,10 +6,9 @@ LOCAL_SRC_FILES := \
     bionic.cpp \
     ui.cpp \
     camera.cpp \
-    buffer.cpp \
-    icu55.c
+    buffer.cpp
 
-LOCAL_SHARED_LIBRARIES := libbinder libui libgui libcutils libutils libc liblog libstagefright_foundation libicuuc libicui18n
+LOCAL_SHARED_LIBRARIES := libbinder libui libgui libcutils libutils libc liblog libstagefright_foundation
 LOCAL_C_INCLUDES += frameworks/av/media/ndk/include
 LOCAL_MODULE := libsprout
 LOCAL_MODULE_TAGS := optional
