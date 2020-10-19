c0a5d6a1df053008af56f7f958db01cef491d766
SamarV-121 <samarvispute121@gmail.com>
2020-10-19 04:52:37 +0000

    sprout: Switch to standard wifi HIDL
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -221,7 +221,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
  PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi@1.0-service \
     libwpa_client \
     hostapd \
     wpa_supplicant \
